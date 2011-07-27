fdbb3f819137f334c564bb722da90552ee4cd51c
Isaac Castillo <icastillo@cbcmail.org>
2011-07-27 03:04:49 -0500

    modified controller to show home page if no slug and to started section to show 404 page if no results.

--- application/classes/controller/site/page.php
+++ application/classes/controller/site/page.php
@@ -5,17 +5,37 @@ class Controller_Site_Page extends Controller_Site_Default {
 	public function action_index()
 	{
 		
-		$this->template->content = View::factory('site/pages/page')
-			->bind('page', $page)
-			->bind('contents', $contents);
-			
 		$slug = $this->request->param('slug');
-		$page = ORM::factory('page')->get_page_with_slug($slug);
 		
-		$contents = $page->contents->find_all();
+		if(empty($slug))
+		{
+			// Show home page if no slug appended
+			$this->template->content = View::factory('site/pages/home');
+		}
+		elseif(!empty($slug))
+		{
+			
+			$page = ORM::factory('page')->get_page_with_slug($slug);
+			
+			// If page exists then load it
+			if($page->loaded())
+			{
+				$this->template->content = View::factory('site/pages/page')
+					->bind('page', $page)
+					->bind('contents', $contents);
+					
+				$contents = $page->contents->find_all();
+
+				$this->template->browser_title = $page->browser_title;
+				$this->template->page_title = $page->page_title;
+			}
+			else
+			{
+				// Page not found send to 404
+				echo 'not found';
+			}
+		}
 		
-		$this->template->browser_title = $page->browser_title;
-		$this->template->page_title = $page->page_title;
 	}
 	
 }
