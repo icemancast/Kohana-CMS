e6a06e2dc0eb398cf2b49a2a3f910876bf6a796a
Isaac Castillo <icastillo@cbcmail.org>
2011-09-15 16:57:50 -0500

    cleaned up lightcast helper.

--- application/classes/helper/lightcast.php
+++ application/classes/helper/lightcast.php
@@ -15,49 +15,6 @@ class Helper_Lightcast {
 			$channel_array = $channel_array['response']['data'];
 			
 			return $channel_array;
-			
-			/*
-			
-			echo 'working...<br /><br />';
-			
-			foreach($channel_array as $array)
-			{
-				$video_db = ORM::factory('sermon');
-				$video_db->where('lightcast_id', '=', $array['id'])->find();
-				
-				if($video_db->loaded())
-				{
-					// Check to see if cupertino is set to 1 and update database
-					if ($array['cupertino'] == 1 && $video_db->cupertino == 0)
-					{
-						$video_db->cupertino = 1;
-						$video_db->save();
-					}
-				}
-				else
-				{
-					$video_db->lightcast_id = $array['id'];
-					$video_db->podcast_id = 1;
-					$video_db->speaker_id = 1;
-					$video_db->title = $array['title'];
-					$video_db->description = $array['description'];
-					$video_db->file_size = $array['size'];
-					$video_db->duration = $array['duration'];
-					$video_db->cupertino = $array['cupertino'];
-					$video_db->lightcast_location = $array['embed']['location'];
-					$video_db->lightcast_code = $array['embed']['code'];
-					$video_db->date = strtotime($array['updated']);
-					
-					$video_db->save();
-					
-				}
-				
-			}
-			
-			echo 'ET phoned home :)';
-			
-			*/
-		
 		}
 		
 	}
