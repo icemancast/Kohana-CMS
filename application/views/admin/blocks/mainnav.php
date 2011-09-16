a405c7a964ff8f3d5329e6cb9e08b06bd1f55da4
Isaac Castillo <icastillo@cbcmail.org>
2011-09-16 13:15:59 -0500

    commented out nav item

--- application/views/admin/blocks/mainnav.php
+++ application/views/admin/blocks/mainnav.php
@@ -23,7 +23,7 @@
 	</li>
 	<li><a href="<?= url::base(); ?>admin/sermon">Sermons</a>
 		<ul>
-			<li><a href="<?= url::base(); ?>admin/sermon/manage">Add a Sermon</a></li>
+			<!-- <li><a href="<?= url::base(); ?>admin/sermon/manage">Add a Sermon</a></li> -->
 			<li><a href="<?= url::base(); ?>admin/podcast">View Podcast Feeds</a></li>
 			<li><a href="<?= url::base(); ?>admin/podcast/manage">Add a Podcast Feed</a></li>
 			<li><a href="<?= url::base(); ?>admin/speaker">View Speakers</a></li>
