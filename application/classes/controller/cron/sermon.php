dfbc18e061a33d2f4fd9b2eb9ebeaf871ffb7836
Isaac Castillo <icastillo@cbcmail.org>
2011-09-16 13:13:51 -0500

    sermon part of website.

--- application/classes/controller/cron/sermon.php
+++ application/classes/controller/cron/sermon.php
@@ -5,7 +5,8 @@ class Controller_Cron_Sermon extends Controller {
 	public function action_index()
 	{
 		if(isset($_SERVER['HTTP_HOST'])) {
-			//exit();
+			// redirect to home page if accessed via browser
+			//$this->request->redirect('/');
 		}
 		
 		$channel = $this->request->param('var', FALSE);
@@ -13,6 +14,10 @@ class Controller_Cron_Sermon extends Controller {
 		
 		$lightcast_sermons = Helper_Lightcast::stream($speaker->console_id);
 		$lightcast_sermons = array_reverse($lightcast_sermons);
+		
+		//var_dump($lightcast_sermons);
+		
+		echo 'working... ';
 		
 		foreach($lightcast_sermons as $lightcast_sermon)
 		{
@@ -23,18 +28,22 @@ class Controller_Cron_Sermon extends Controller {
 			{
 				$values['lightcast_id'] = $lightcast_sermon['id'];
 				$values['speaker_id'] = $speaker->id;
-				$values['title'] = $lightcast_sermon['title'];
+				$values['sermon_title'] = $lightcast_sermon['title'];
 				$values['description'] = $lightcast_sermon['description'];
 				$values['file_size'] = $lightcast_sermon['size'];
+				$values['duration'] = $lightcast_sermon['duration'];
 				$values['cupertino'] = $lightcast_sermon['cupertino'];
 				$values['lightcast_location'] = $lightcast_sermon['embed']['location'];
 				$values['lightcast_code'] = $lightcast_sermon['embed']['code'];
+				$values['date_sermon'] = strtotime($lightcast_sermon['updated']);
 
 				$sermon->values($values);
 				$sermon->save();
 			}
 		}
-					
+		
+		echo 'done :)';			
+	
 	}
 	
 }
