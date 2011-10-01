f56b13ce621901186f3b7098dcdbb54bcfa3789f
Isaac Castillo <icastillo@cbcmail.org>
2011-10-01 13:27:00 -0500

    started setup of layout for main site page.

--- application/views/site/layout/layout.php
+++ application/views/site/layout/layout.php
@@ -5,19 +5,21 @@
 <meta charset="utf-8" />
 <title><?= $browser_title; ?></title>
 
-<link rel="stylesheet" href="media/site/css/reset.css" />
 <link href='http://fonts.googleapis.com/css?family=Give+You+Glory&v2' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="media/site/css/layout.css" />
+<?php foreach ($styles as $style): ?>
+<link rel="stylesheet" type="text/css" media="<?= $style['media']; ?>" href="<?= URL::base() ?>media/site/css/<?= $style['name']; ?>.css" />
+<?php endforeach; ?>
+
+<?php foreach ($scripts as $script): ?>
+<script type="text/javascript" src="<?= $script; ?>"></script>
+<?php endforeach; ?>
 
 </head>
 
 <body>
-	<div id="wrapper">
+	<div id="wrapper" class="container_12">
 		<h1><?= $page_title; ?></h1>
 		<p><?= $content; ?></p>
-		
-		
-		
 	</div>
 </body>
 </html>
