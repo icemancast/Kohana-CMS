01e467904ae4cf57a8f103322e8d8b9cb27a771f
Isaac Castillo <icastillo@cbcmail.org>
2011-10-18 15:32:46 -0500

    home and content layout.

--- application/views/site/layout/home.php
+++ application/views/site/layout/home.php
@@ -14,6 +14,15 @@
 <script type="text/javascript" src="<?= $script; ?>"></script>
 <?php endforeach; ?>
 
+<script type="text/javascript">
+/*<![CDATA[*/
+	// onload
+	$(function() {
+		$("body").addGrid(12, {img_path: '<?= URL::base(); ?>media/site/images/'});
+	});
+/*]]>*/
+</script>
+
 </head>
 
 <body>
@@ -22,21 +31,34 @@
 			<a href="<?= URL::base(); ?>" id="brand"></a>
 			
 			<!-- timer code -->
+			<div id="online-service"></div>
 			
 			<!-- search bar -->
-		</div>
-		<ul id="nav" class="container_12">
-			<li>test</li>
-			<li>adsf</li>
-		</ul>
+			<?= $search ?>
+			
+		</div><!--end of header -->
 		
-		<ul id="left-nav">
-			<li>adsf</li>
-			<li>adsf</li>
-		</ul>
+		<?= $nav; ?>
 		
-		<h1><?= $page_title; ?></h1>
-		<p><?= $content; ?></p>
-	</div>
+		<span class="clear"></span>
+		
+		<?= $leftnav; ?>
+		
+		<div id="photos" class="grid_9">
+			<div class="grid_5 alpha left-nav-content">
+				<p>test this stuff</p>
+			</div>
+			<img src="<?= URL::base(); ?>media/site/images/content/headers/header.jpg" height="454" width="738" />
+		</div><!--end of slidshow -->
+		
+		<div id="banners" class="grid_12">
+			<?= $banners; ?>
+		</div><!--end of banners -->
+		
+		<div id="footer" class="grid_12">
+		 <?= $footer; ?>
+		</div><!--end of footer -->
+		
+	</div><!--end of wrapper -->
 </body>
 </html>
