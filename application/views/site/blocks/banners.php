1c5bae6cb35a6e01622f56dc22a612d386578734
Isaac Castillo <icastillo@cbcmail.org>
2011-10-18 15:32:32 -0500

    added blocks to site user side.

---
+++ application/views/site/blocks/banners.php
@@ -0,0 +1,8 @@
+<a href="#"><img src="<?= URL::base() ?>media/site/images/content/banners/banner-1.jpg" class="grid_3 alpha" /></a>
+<a href="#"><img src="<?= URL::base() ?>media/site/images/content/banners/banner-2.jpg" class="grid_3" /></a>
+<a href="#"><img src="<?= URL::base() ?>media/site/images/content/banners/banner-3.jpg" class="grid_3" /></a>
+<div class="grid_3 omega banner-slideshow">
+	<a href="#"><img src="<?= URL::base() ?>media/site/images/content/banners/banner-4.jpg" /></a>
+	<a href="#"><img src="<?= URL::base() ?>media/site/images/content/banners/banner-2.jpg" /></a>
+	<a href="#"><img src="<?= URL::base() ?>media/site/images/content/banners/banner-1.jpg" /></a>
+</div>
