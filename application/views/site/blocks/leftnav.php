aae506c951d304198778f3c644946bf990bc5def
Isaac Castillo <icastillo@cbcmail.org>
2011-10-24 09:18:50 -0500

    left navigation

--- application/views/site/blocks/leftnav.php
+++ application/views/site/blocks/leftnav.php
@@ -1,25 +1,13 @@
-	<ul id="left-nav" class="grid_3">
-		<li class="pastors-home-link">
-			12 Services<br />
-			3 Preachers<br />
-			Your Choice<br />
-			What a Church
-		</li>
-		<li><a href="#" class="show-hide">Watch/Listen to Sermons</a></li>
-		<li><a href="#">Service Times</a></li>
-		<li><a href="#">Location</a></li>
-		<li><a href="#">Give Online</a></li>
-		<li><a href="#">I saw it in the Bulletin</a></li>
-		<li class="newsletter-home">
-			<form id="highfive" name="ccoptin" action="http://visitor.constantcontact.com/d.jsp" target="_blank" method="post">
-			<h2>Sign up for Robert's Newsletter</h2>
-			<p>
-				<input id="newsletter" type="text" name="ea" size="22" value="Sign up for newsletter">
-				<input type="hidden" name="Join" value="Join">
-				<input type="hidden" name="m" value="1101966345173">
-				<input type="hidden" name="p" value="oi">
-				<input type="image" src="<?= URL::base() ?>media/site/images/layout/button-go.jpg">
-			</p>
-			</form>
-		</li>
-	</ul><!--end of left nav -->
+<ul id="left-nav" class="grid_3">
+<?php
+
+echo $page->nav->nav_title;
+
+echo '<br /><br />';
+
+foreach ($url as $link)
+{
+	echo $link->url_title;
+}
+?>
+</ul>
