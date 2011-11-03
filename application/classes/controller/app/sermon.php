7484650897114f76397a18f1e164fad040e723ce
Isaac Castillo <icastillo@cbcmail.org>
2011-11-02 19:21:44 -0500

    json for sermons
    still working on this controller for sermons to json data

--- application/classes/controller/app/sermon.php
+++ application/classes/controller/app/sermon.php
@@ -5,13 +5,22 @@ class Controller_App_Sermon extends Controller {
 	public function action_index()
 	{
 		
+		##### USE JSON_ENCODE 
+		##### AND USE $this->response->body($json); to output
+		
+		
+		$this->response->headers(array('Content-Type' => 'application/json', 'Cache-Control' => 'no-cache'));
+		
 		$load_sermon = ORM::factory('sermon');
 		//$sermons = $load_sermon->where('cupertino', '=', '1')->find_all();
 		
 		$sermons = $load_sermon->find_iphone_video();
 		//$sermon_count = $sermons->count_all();
 		
-		$json = '{"basePageUrl":"bundle://App.bundle","sermons":[';
+		$json = '{
+		"responseCode" : "0",
+		"model" : [
+		';
 		
 		foreach($sermons as $sermon)
 		{
@@ -19,47 +28,17 @@ class Controller_App_Sermon extends Controller {
 			$imagePath = 'http://cupertino.lightcastmedia.com/cbcmedia/' . $sermon->lightcast_id . '/thumbnail.jpg';
 			$pageUrl = 'http://customers.lightcastmedia.com/cupertino/3073/' . $sermon->lightcast_id . '.m3u8';
 			
-			$title = Helper_Json::clean_json_value($sermon->title);
+			$title = Helper_Json::clean_json_value($sermon->sermon_title);
 			$speaker = Helper_Json::clean_json_value($sermon->speaker);
 			$description = Helper_Json::clean_json_value($sermon->description);
 			
-			$json .= '{"title": ' . $title . ',"speaker": ' . $speaker . ',"description": ' . $description . ',"duration": "' . $sermon->duration . '","date": "' . $sermon->date . '","imagePath": "' . $imagePath . '","pageURL": "' . $pageUrl . '"},';
+			$json .= '{"title": ' . $title . ',"speaker": ' . $speaker . ',"description": ' . $description . ',"duration": "' . $sermon->duration . '","date": "' . $sermon->date_sermon . '","imagePath": "' . $imagePath . '","pageURL": "' . $pageUrl . '"},';
 			
 		}
 		
 		$json .= ']}';
 			
 		echo $json;
-		
-		//echo json_encode($sermon_array);
-		
-		// http://cupertino.lightcastmedia.com/cbcmedia/37950/thumbnail.jpg
-		// http://customers.lightcastmedia.com/cupertino/3073/37950.m3u8
-		
-		
-		// {
-		// 										"basePageUrl":"bundle://App.bundle",
-		// 										"sermons": 
-		// 										[
-		// 											{
-		// 												"title": "Sermon 1",
-		// 												"imagePath": "media.png",
-		// 												"pageURL": "http://customers.lightcastmedia.com/cupertino/3073/37950.m3u8"
-		// 											},
-		// 											{
-		// 												"title": "Sermon 2",
-		// 												"imagePath": "media.png",
-		// 												"pageURL": "http://customers.lightcastmedia.com/cupertino/3073/37950.m3u8"
-		// 											},
-		// 											{
-		// 												"title": "Sermon 3",
-		// 												"imagePath": "media.png",
-		// 												"pageURL": "http://customers.lightcastmedia.com/cupertino/3073/37950.m3u8"
-		// 											}
-		// 										]
-		// 										}
-		
-		
 		
 	}
 	
