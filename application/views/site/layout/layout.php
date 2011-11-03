a8c949f0e79e82ded4d2e720a2d67ca12e71c138
Isaac Castillo <icastillo@cbcmail.org>
2011-11-02 19:18:22 -0500

    layout adjustments
    modification of layout.

--- application/views/site/layout/layout.php
+++ application/views/site/layout/layout.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8" />
 <title><?= $browser_title; ?></title>
 
-<link href='http://fonts.googleapis.com/css?family=Give+You+Glory&v2' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
 <?php foreach ($styles as $style): ?>
 <link rel="stylesheet" type="text/css" media="<?= $style['media']; ?>" href="<?= URL::base() ?>media/site/css/<?= $style['name']; ?>.css" />
 <?php endforeach; ?>
@@ -42,11 +42,38 @@
 		
 		<span class="clear"></span>
 		
-		<?= $leftnav; ?>
+		<div id="inner-wrapper" class="grid_12">
 		
-		<h1><?= $page_title; ?></h1>
+			<?php 
 		
-		<?= $content; ?>
+				// Left navigation
+				echo $leftnav;
+		
+				// Check if image headers and if so make slide show
+				if(!empty($photos))
+				{
+					echo '		<div class="photo-slideshow grid_9">
+						';
+					foreach($photos as $photo)
+					{
+						echo '<img src="' . $photo . '" height="300" width="738" />
+							';
+					}
+					echo '</div><!--end of photo slideshow -->
+						';
+				}
+			?>
+		
+			<div class="content grid_9 omega">
+				
+				<h1 class="grid_9"><?= $page_title; ?></h1>
+		
+				<?= $content; ?>
+				
+			</div><!--end of content -->
+			
+		</div><!--end of inner wrapper -->
+		
 		
 		<div id="footer" class="grid_12">
 		 <?= $footer; ?>
