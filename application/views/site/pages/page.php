44aea38e118afa533014db98ccf8942a32ac1413
Isaac Castillo <icastillo@cbcmail.org>
2011-11-02 19:19:53 -0500

    content area
    iterates for all contents that belong to the pages.

--- application/views/site/pages/page.php
+++ application/views/site/pages/page.php
@@ -2,6 +2,9 @@
 
 	foreach($contents as $content)
 	{
-		echo $content->content_title . '<br /><br />';
-		echo $content->content;
+		
+		echo '<div class="content-block grid_9">';
+			echo '<h2>' . $content->content_title . '</h2>';
+			echo $content->content;
+		echo '</div>';
 	};
