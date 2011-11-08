96224feda71497e07da62aa76a6d79b72514c899
Isaac Castillo <icastillo@cbcmail.org>
2011-11-08 11:20:10 -0600

    code clean up. deleted comments and code not needed

--- application/classes/controller/app/sermon.php
+++ application/classes/controller/app/sermon.php
@@ -5,17 +5,10 @@ class Controller_App_Sermon extends Controller {
 	public function action_index()
 	{
 		
-		##### USE JSON_ENCODE 
-		##### AND USE $this->response->body($json); to output
-		
-		
 		$this->response->headers(array('Content-Type' => 'application/json', 'Cache-Control' => 'no-cache'));
 		
-		$load_sermon = ORM::factory('sermon');
-		//$sermons = $load_sermon->where('cupertino', '=', '1')->find_all();
-		
+		$load_sermon = ORM::factory('sermon');		
 		$sermons = $load_sermon->find_iphone_video();
-		//$sermon_count = $sermons->count_all();
 		
 		$json_array = array();
 		$json_array['responseCode'] = '0';
