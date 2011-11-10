7ac5cc7aa9102772312e4861555046fbcb5de959
Isaac Castillo <icastillo@cbcmail.org>
2011-11-09 21:43:37 -0600

    uncommented unittest module and added if statement for running it

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -105,7 +105,7 @@ Kohana::modules(array(
 	'image'      => MODPATH.'image',      // Image manipulation
 	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	'f1'        => MODPATH.'f1',        // Object Relationship Mapping
-	// 'unittest'   => MODPATH.'unittest',   // Unit testing
+	'unittest'   => MODPATH.'unittest',   // Unit testing
 	'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 ));
 
