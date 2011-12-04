170b431982d6c3b749d2df86309a6f1e0a76b91f
Isaac Castillo <icastillo@cbcmail.org>
2011-12-04 15:53:13 -0600

    content tags from datase, e.g [tag]

--- application/config/content.php
+++ application/config/content.php
@@ -4,5 +4,7 @@ return array(
 	'short_codes' => array(
 		'[URL_BASE]'	=> URL::base(),
 		'[APPPATH]' 	=> APPPATH,
+		'[IMAGES]' => 'http://images.communitybible.com/',
+		'[S3-VIDEOS]' => 'http://videos.communitybible.com/',
 	)
-);
+);	
