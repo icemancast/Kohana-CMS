0409415b8d872291fd15d59d71e37df7a6fa39f1
Isaac Castillo <icastillo@cbcmail.org>
2011-12-19 10:31:04 -0600

    getting rid of 960 helper for now.

--- application/classes/controller/site/default.php
+++ application/classes/controller/site/default.php
@@ -37,19 +37,18 @@ abstract class Controller_Site_Default extends Controller_Template {
 				URL::base() . 'media/site/js/plugin.jquery.cycle.lite.js',
 				URL::base() . 'media/site/js/plugin.jquery.toggleValue.min.js',
 				URL::base() . 'media/site/js/common.js',
-				URL::base() . 'media/site/js/dev-plugin.jquery.960grid-1.0.js',
+				// URL::base() . 'media/site/js/dev-plugin.jquery.960grid-1.0.js',
 			);
 			
 			// Production scripts
 			/*
 			$this->template->scripts 		= array(
 				'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js',
-				URL::base() . 'media/site/js/jquery.plugins.js',
 				URL::base() . 'media/site/js/common.js',
 			);*/
 			
 			$this->template->head_code		= ''; // Not sure if i need this
-			$this->template->nav			= View::factory('site/blocks/nav')->render();
+			// $this->template->nav			= View::factory('site/blocks/nav')->render();
 			$this->template->leftnav		= View::factory('site/blocks/homeleftnav')->render();
 			$this->template->search			= View::factory('site/blocks/search')->render();
 			$this->template->banners		= View::factory('site/blocks/banners')->render();
