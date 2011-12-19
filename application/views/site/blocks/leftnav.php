f3afa5ece18c120e2bf91d7fb68482a49500c51a
Isaac Castillo <icastillo@cbcmail.org>
2011-12-19 10:32:37 -0600

    fixed left nab so it wouldn't have 1st link as duplicate.

--- application/views/site/blocks/leftnav.php
+++ application/views/site/blocks/leftnav.php
@@ -1,15 +1,26 @@
 <ul id="left-nav" class="alpha grid_3">
 <?php
 
-echo '		<li class="nav-title"><a href="' . URL::base() . $page->slug . '">' . $page->nav->nav_title . '</a></li>
-	';
+$i = 1;
 
 foreach ($url as $link)
 {
-	// check if current page
-	$class = $page->slug == $link->url ? ' class="current-uri" ' : '';
-	echo '	<li><a href="' . URL::base() . $link->url . '"' . $class . '>' . $link->url_title . '</a></li>
-	';
+	if ($i != 1)
+	{
+		
+		// check if current page
+		$class = $page->slug == $link->url ? ' class="current-uri" ' : '';
+		echo '	<li><a href="' . URL::base() . $link->url . '"' . $class . '>' . $link->url_title . '</a></li>
+		';
+	
+	}
+	else
+	{
+		echo '	<li class="nav-title"><a href="' . URL::base() . $link->url . '">' . $link->url_title . '</a></li>';
+	}
+	
+	$i++;
+	
 }
 ?>
 	</ul>
