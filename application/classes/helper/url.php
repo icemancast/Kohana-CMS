5123e51c9552706407e6dc1ae0f004c082501d82
Isaac Castillo <icastillo@cbcmail.org>
2011-12-19 10:32:55 -0600

    started url helper to help with formatting links.

---
+++ application/classes/helper/url.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access');
+
+class Helper_Url {
+	
+	public static function setup($url)
+	{
+		// Check to see if full url path if not append local domain.com/slug
+		// preg match find if url has http:// or https://
+		
+		
+		// Check to see if url has 3 hash ### tags and remove it. it might be main nav
+	}
+}
