1b8f65857b5249d9fa17627356f0d27df4d5a26b
Isaac Castillo <icastillo@cbcmail.org>
2012-01-12 08:27:16 -0600

    capistrano config file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,21 +7,42 @@ set :deploy_to, "/var/www/"
 set :scm, "git"
 set :branch, "master"
 set :git_enable_submodules, 1
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db, domain, :primary => true # This is where Rails migrations will run
-role :db, domain
+#role :db, domain
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# Add symlinks for config files
+# Current links are:
+# database.php, aws.config.inc.php, .htaccess, bootstrap.php
+#
+# Need to remove the following
+# logs, cache
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  desc "Restart web server"
+  task :restart do
+    run "/etc/init.d/apache2 restart"
+  end
+
+  desc "Create all symlinks"
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/database.php #{current_path}/application/config/database.php"
+    run "ln -nfs #{shared_path}/config/aws.config.inc.php #{current_path}/application/classes/helper/vendor/awssdk/config.inc.php"
+    run "ln -nfs #{shared_path}/settings/bootstrap.php #{current_path}/application/bootstrap.php"
+    #run "ln -nfs #{shared_path}/settings/.htaccess #{current_path}/.htaccess"
+    run "ln -nfs #{shared_path}/cache #{current_path}/application/cache"
+    run "ln -nfs #{shared_path}/logs #{current_path}/application/logs"
+  end
+  
+  desc "Delete unwanted files and folders"
+  task :setupsite do
+    run "rm -rf #{current_path}/.htaccess #{current_path}/DEVELOPERS.md #{current_path}/LICENSE.md #{current_path}/README.md #{current_path}/TESTING.md #{current_path}/application/logs #{current_path}/application/cache #{current_path}/application/config/database.php.template #{current_path}/application/config/f1.php.template"
+    run "cp #{shared_path}/settings/.htaccess #{current_path}/.htaccess"
+  end
+  
+end
+
+after "deploy:update_code", "deploy:setupsite"
+after "deploy", "deploy:cleanup"
